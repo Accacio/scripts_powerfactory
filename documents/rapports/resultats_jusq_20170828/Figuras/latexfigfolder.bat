1bef75eff06a6a6a41fb633735d6c1cba93dbdfe
Accácio <raccacio@poli.ufrj.br>
2017-08-30 17:54:01 +0200

    Creation of folder Documents
    Creation of folder Documents
    Changes in the Rapport

--- documents/rapports/resultats_jusq_20170828/Figuras/latexfigfolder.bat
+++ documents/rapports/resultats_jusq_20170828/Figuras/latexfigfolder.bat
@@ -1,11 +1,12 @@
 @echo off
-set /p id= "Qual o nome da pasta onde estão as figuras?"
+set /p id= "Qual o nome da pasta onde estao as figuras?"
 if exist %id%.txt (
     del %id%.txt
 )
 set /p opc= "H ou h?"
 FOR %%f in (.\%id%\*.pdf .\%id%\*.png .\%id%\*.jpg) DO (echo \begin{figure}[%opc%]
 echo 	\begin{center}	
+echo		\captionsetup{justification=centering,margin=.5cm}
 echo 		\includegraphics[width=8cm]{%id%/%%~nxf}
 echo 		\caption{\todo{escrevaaquiseucaption}}
 echo 		\label{fig:%%~nf}
