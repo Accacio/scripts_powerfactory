069f404a5f38b20c4eccbb4b7401555b62c1a638
Accácio <raccacio@poli.ufrj.br>
2017-09-06 18:27:55 +0200

    Changes Latex

--- matlab/models/modeltest.m
+++ matlab/models/modeltest.m
@@ -1,7 +1,6 @@
 function [t, x, y] = modeltest
-global u a pmax x1
-
-options = simget('modeltest');
+global u a pmax x1 Ent Sor
+options = simget('modeltest_model');
 options = simset('InitialState', [x1]);
-[t, x, y] = sim('modeltest', [], options);
-
+% options.InitialState=x1
+[t, x, y] = sim('modeltest_model', [], options);
