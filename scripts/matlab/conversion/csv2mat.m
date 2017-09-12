d3326e5ba67d1327217a42a779e5dc2f34e5289c
Accácio <raccacio@poli.ufrj.br>
2017-09-12 16:28:43 +0200

    Scripts in matlab to print pdf
    Update in data

--- scripts/matlab/conversion/csv2mat.m
+++ scripts/matlab/conversion/csv2mat.m
@@ -1,18 +1,29 @@
 clear 
-% Get File
-values = dlmread('..\..\data\output\csv\alldata.csv',';',2,0);
+clc
+%% Get File
+default_input_folder='..\..\..\data\output\csv\simulations\';
+[FileName,PathName] = uigetfile('*.csv','Select CSV file with simulation data',default_input_folder);
+FileName=FileName(1:end-4);
+
+%% Process File
+values = dlmread([PathName FileName '.csv'],';',2,0);
 values_size=size(values);
-fileID = fopen('..\..\data\output\csv\alldata.csv','r','n','ISO-8859-15');
+fileID = fopen([PathName FileName '.csv'],'r','n','ISO-8859-15');
 element = textscan(fileID,'%q',values_size(2),'delimiter',';');
 properties = textscan(fileID,'%q',values_size(2),'delimiter',';');
 
 % Create structure with csv data
 for i = [1:values_size(2)]
-    eval(['all_data.' regexprep(element{1,1}{i,1},{'\.';':';'-';' ';'\'},{'__';'___';'____';'_____';'______'}) ...
+    eval([FileName '.' regexprep(element{1,1}{i,1},{'\.';':';'-';' ';'\'},{'__';'___';'____';'_____';'______'}) ...
                   '.' regexprep(properties{1,1}{i,1},{'\.';':';'-';' ';'\'},{'__';'___';'____';'_____';'______'}) '=values(1:end,i);'])
 end
-clearvars -except all_data
+eval(['clearvars -except ' FileName ' FileName PathName' ])
+newPathName=strrep(PathName,'csv','matlab');
+if ~exist(newPathName,'dir')
+    mkdir(newPathName);
+end
+%%Save File    
+[saveFileName,PathName]=uiputfile('*.mat','Save data as',[newPathName FileName]);
+save([PathName saveFileName], FileName);
+clear
 
-% Save structure in .mat
-% save('..\..\data\output\matlab\all_data')
-save('all_data')
--- scripts/matlab/print/printpdf.m
+++ scripts/matlab/print/printpdf.m
@@ -1,34 +1,78 @@
+%% Load Data
+clear
+clc
+default_input_folder='..\..\..\data\output\matlab\simulations\';
+[FileName,PathName] = uigetfile('*.mat','Select mat file with simulation data',default_input_folder);
 
-figure 
-plot(all_data.All_____calculations.b___tnow_____in_____s,...
-    all_data.C_____2____29_____MT_____ind.m___Psum___bus1_____in_____MW,...
-    all_data.All_____calculations.b___tnow_____in_____s,...
-    all_data.C_____2____29_____MT_____ind.m___Qsum___bus1_____in_____Mvar)
-n_title='Puissance Active et Reactive de la Charge C2.29 MT';
-title(n_title,'interpreter','latex')
-legend('Puissance Active','Puissance Reactive')
+if((strcmp(FileName,0))||((strcmp(PathName,0))))
+    return;
+end
 
-set(gcf, 'PaperPosition', [0 0 8 8]) 
-set(gca,'FontSize',8) 
-set(gcf, 'PaperSize', [8 8]) 
-saveas(gcf,['' regexprep(n_title,{'\.';' ';':';'\';'/';'?';'"';'<';'>';'|'},'_') '.pdf'])
+FileName_noext=FileName(1:end-4);
+eval(['load(''' PathName FileName ''');'])
+eval(['properties=fieldnames(' FileName_noext ');'])
+properties_no_time=properties(2:end);
 
+%% Choose what to plot
+element=[];
+test='';
+choice='Yes';
+ndata=0;
+while(strcmp(choice,'Yes'))
+    
+    i=listdlg('PromptString','Select data:','SelectionMode','Single','ListString',properties_no_time);
+    if(isempty(i))
+        clear
+        return;
+    end
+    eval(['element=[fieldnames(' FileName_noext '.' properties_no_time{i} ')]'])
+    j=listdlg('PromptString','Select data:','SelectionMode','Single','ListString',element);
+    if(isempty(i))
+        clear
+        return;
+    end
+    
+    if(isempty(test))
+    test=[FileName_noext '.All_____calculations.b___tnow_____in_____s,' FileName_noext '.' properties_no_time{i} '.' element{j}]
 
-%%
-figure 
-plot(all_data.All_____calculations.b___tnow_____in_____s,...
-    all_data.N21______N21_____DG.m___u1_____in_____p__u__,...
-    all_data.All_____calculations.b___tnow_____in_____s,...
-    all_data.N23______N23_____DG.m___u1_____in_____p__u__,...
-    all_data.All_____calculations.b___tnow_____in_____s,...
-    all_data.N29______N29_____DG.m___u1_____in_____p__u__)
-n_title='Tension des Bus N21 N23 et N29';
-title(n_title,'interpreter','latex')
-legend('N21','N23','N29')
+    else
+        test=[test ',' FileName_noext '.All_____calculations.b___tnow_____in_____s,' FileName_noext '.' properties_no_time{i} '.' element{j}]
+    end
+    
+    ndata=ndata+1;
+    choice = questdlg('Would you like to plot any other data?','Dessert Menu','Yes','No','No');
+end
 
-set(gcf, 'PaperPosition', [0 0 8 8]) 
-set(gca,'FontSize',8) 
-set(gcf, 'PaperSize', [8 8]) 
-saveas(gcf,['' regexprep(n_title,{'\.';' ';':';'\';'/';'?';'"';'<';'>';'|'},'_') '.pdf'])
+figure
+eval(['plot(' test ')'])
+prompt = {'Enter title:'};
+defaultans={'Title'};
+for i=[1:ndata]
+prompt={prompt{1:end} ['Enter legend n° ' num2str(i)]};
+defaultans = {defaultans{1:end} ['Legend  n°' num2str(i)'] };
+end
+input=inputdlg(prompt,'Input',ndata,defaultans);
+if(isempty(input))
+    close gcf;
+    clear
+    return;
+end
+n_title=strjoin(input(1));
 
+title(n_title,'interpreter','latex');
+legend(input(2:end));
 
+set(gcf, 'PaperPosition', [0 0 8 8]);
+set(gca,'FontSize',6); 
+set(gcf, 'PaperSize', [8 8]); 
+FileName=regexprep(n_title,{'\.';' ';':';'\';'/';'?';'"';'<';'>';'|'},'_');
+
+PathName=strrep(PathName,'matlab','img');
+if ~exist(PathName,'dir')
+    mkdir(PathName);
+end
+
+%% Save File    
+[saveFileName,PathName]=uiputfile('*.pdf','Save data as',[PathName FileName]);
+saveas(gcf,[ PathName FileName '.pdf'])
+clear
