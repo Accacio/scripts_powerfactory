ff3ee5511ad62755a312624b3e1e5f70a57c57e7
Accácio <raccacio@poli.ufrj.br>
2017-09-18 18:05:06 +0200

    Rapport

--- power_factory/networks/model_with_discrete_control/modeltest.m
+++ power_factory/networks/model_with_discrete_control/modeltest.m
@@ -3,20 +3,20 @@ global u a pmax
 % options = simget('modeltest_model');
 % options = simset('InitialState', [x1]);
 
-g=u;
-QM=0.4*pmax;
-Qm=-0.4*pmax;
-UM=22;
-Um=18;
-U1=19;
-U2=19.25;
-U3=20.75;
-U4=21;
-y=QM*(g<U1)+(g-U2)*((QM)/(U1-U2)).*(g<=U2&g>=U1)...
-    +(g-U3)*((-Qm)/(U3-U4)).*(g<=U4&g>=U3)+Qm.*(g>U4)
-y=y'
+% g=u;
+% QM=0.4*pmax;
+% Qm=-0.4*pmax;
+% UM=22;
+% Um=18;
+% U1=19;
+% U2=19.25;
+% U3=20.75;
+% U4=21;
+% y=QM*(g<U1)+(g-U2)*((QM)/(U1-U2)).*(g<=U2&g>=U1)...
+%     +(g-U3)*((-Qm)/(U3-U4)).*(g<=U4&g>=U3)+Qm.*(g>U4)
+% y=y'
 % plot(g,y)
-x=[];
-t=[0 0.01]'
+% x=[];
+% t=[0 0.01]'
 
-%  [t, x, y] = sim('modeltest_model');
+ [t, x, y] = sim('modeltest_model');
